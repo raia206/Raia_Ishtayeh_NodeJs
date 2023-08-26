9445c83a0920578536a9340878bfea864ba42852
raia ishtayeh <raia2020.a@gmail.m>
2023-08-26 12:39:59 +0300

    node js project

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 2000;
@@ -20,4 +20,23 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
+});*/
+
+const http = require('http');
+const fs = require('fs');
+const PORT = 1000;
+const server = http.createServer((req, res) => {
+  const log = `${new Date().toISOString()} - ${req.url}\n`;
+ 
+  fs.appendFile('requests.txt', log, (err) => {
+    if (err) {
+      console.error('Error writing to requests.txt:', err);
+    }
+  });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Request log successfully!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port http://localhost:${PORT}`);
 });
